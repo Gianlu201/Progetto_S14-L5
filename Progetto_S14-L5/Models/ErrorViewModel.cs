bf88315c7e88a81ad04d3ee9c4e0eae54a46ae1d
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-21 09:32:55 +0100

    Aggiungere i file di progetto.

---
+++ Progetto_S14-L5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progetto_S14_L5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
