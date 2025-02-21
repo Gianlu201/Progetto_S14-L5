7eb2eaa58f9642c6d0525b6b5586c618720dbf77
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-21 16:29:57 +0100

    Edit options implemented

--- Progetto_S14-L5/Models/Shoes.cs
+++ Progetto_S14-L5/Models/Shoes.cs
@@ -17,6 +17,8 @@
             "https://img.freepik.com/vettori-gratuito/le-scarpe-da-tennis-moderne-di-sport-di-colore-blu-con-i-laccetti-bianchi-realizzano-la-singola-immagine-sull-illustrazione-isolata-fondo-bianco_1284-31208.jpg?t=st=1740129627~exp=1740133227~hmac=667a6febf15b3079011c7a13213e662160409b9981c63de82df14184f461f503&w=400",
             "https://assets.adidas.com/images/w_400,f_auto,q_auto/284a892018974be28d6be73d110e0d03_9366/Scarpe_Falcon_Rosa_IE8203.jpg",
             "https://www.bfgcdn.com/1500_1500_90/123-1195-0511/the-north-face-womens-vectiv-enduris-3-scarpe-per-trail-running.jpg",
+            "https://www.misterrunning.com/media/products/2023-media-07/the-north-face-vectiv-enduris-3-scarpe-trail-donna-asphalt-grey-nf0a7w5pqn2_A-600x600.jpg",
+            "https://image-raw.reversible.com/raw_images/717d5b072f1bd402ef008cad8c54169ea8c4b606d07b3bb08bd8b51eac376353",
         ];
     }
 }
